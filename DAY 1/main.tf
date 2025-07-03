921aa4f2778fa9c38084a73ac938cd32b359ce5f
harsh028-raj <74181011+harsh028-raj@users.noreply.github.com>
2025-07-03 09:54:42 +0530

    Update main.tf
    AWS instance creation

--- DAY 1/main.tf
+++ DAY 1/main.tf
@@ -1,1 +1,10 @@
+# First Terraform file to create basic EC2 instance
 
+provider "aws" {
+    region = "ap-south-1"
+}
+
+resource "aws_instance" "first_resource" {
+    ami = "ami-0d03cb826412c6b0f"
+    instance_type = "t2.micro"
+}
